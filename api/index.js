2e627de6253867e5f27295db480ff8cdb439b734
YohanNayanajith <yohannayanajith40@gmail.com>
2022-07-12 02:17:54 +0530

    After backend part

--- api/index.js
+++ api/index.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 const app = express();
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
 
 dotenv.config();
 
@@ -19,6 +22,9 @@ mongoose
 app.use(express.json());
 app.use("/api/v1/auth",authRoute);
 app.use("/api/v1/user",userRoute);
+app.use("/api/v1/products",productRoute);
+app.use("/api/v1/cart",cartRoute);
+app.use("/api/v1/orders",orderRoute);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000");
